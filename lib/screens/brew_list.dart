f21d34b6d73c2077946ffbfddc89668c30891afc
tnaz <tullah.nazari@gmail.com>
2019-11-22 07:40:39 -0600

    adding task list to task tiles

--- lib/screens/brew_list.dart
+++ lib/screens/brew_list.dart
@@ -2,6 +2,8 @@ import 'package:appdemowithfirebase/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'brew_tile.dart';
+
 class BrewList extends StatefulWidget {
   @override
   _BrewListState createState() => _BrewListState();
@@ -11,12 +13,12 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    //print(brews.documents);
-    brews.forEach((brew) {
-      print(brew.name);
-      print(brew.sugars);
-      print(brew.strength);
-    });
-    return Container();
+
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
